857c8ce415ac8220c7f3f2e1c4cb0aff58ae100f
clauddio-silva <klauddio.silva@gmail.com>
2021-11-21 17:19:27 -0300

    adicao do servidor desenvolvimento no metodo run

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ from src.main.config import app, api
 import os
 
 if __name__ == "__main__":
-    port = int(os.getenv('PORT'))
+    port = int(os.getenv('PORT'), '5000')
     api.init_app(app)
     app.run(host='0.0.0.0', port=port)
